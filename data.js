8ca11fde333093d7070953c78a9e5ab08b09d8d4
Michael <pxmyhdev@gmail.com>
2020-09-27 10:15:46 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const main = {
   name: 'Michael Hu',
   mail: 'michael.yue.hu@gmail.com',
-  img: 'https://public.boxcloud.com/api/2.0/internal_files/718549630058/versions/764017838858/representations/jpg_paged_2048x2048/content/1.jpg?access_token=1!vXANYa7IxAK3njjbIMtHukPwjA28sevy4QM9BhrK1M0NQjpC_xe2j-0O3dBYUkQoQhPCjsvTq7gkId7v-PUxpYww7F8ydRJEALgsODziCUelJo9SB4LAByzjxV1LSpDJMcLefQlFRz6jtiUd-xALZGW1quecjnyFaAuhtC3za8cI1sPw3l4gqcf8KMiXGEaB9N9kzaOJYiZFLhe3mptnv92Hk0pRfHxfuqQnoYOm2tyxKKVPgxg3-eQIPvz0ekXzSQo-RkP9Lwc2Kt0R7iaAYLCmNmlpPqK61Tzdelfk1l_jHzYwZqV_QSA6AowhpJooIjoQoJCZ4s-E5Qljs9ZAiTccBeAO-CTKbt4BUWXD4Zsx-CEQ8FiW9oKZnCk-IoTls1cZsR8Wp-qhklUF9oIn1zB-sAebcdbReiMfTUIwOek8aenRNTiMtEm8AxmTmXup6Zlu2bK5w91j7BbdeGocpKBE9b9XxqmHidEN1RfkqmwMV8gSnW20gaJHOCtGd0iL-dHmVjS9GxVFY5jwwaglZCI-u619QTzJmCWVNgascnHquIXyhvBBT5KNZENGosSAWxxOd9zC5aN55lpFJPgidU4ybrhOZaKyb7ZU1hkWVyG7m_qsqYpfVKuWk3BqvXMxggakSTDGlKbXnzFXGYtLknk8DRJvbceI4w3SuWBDthN3gQ..&box_client_name=box-content-preview&box_client_version=2.49.1',
+  img: 'https://i.ibb.co/DgxjvZn/profile.jpg',
   role: ['Full Stack Engineer/Cloud Developer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
   connects: [
